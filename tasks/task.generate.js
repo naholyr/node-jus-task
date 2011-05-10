c89aee9e701c5071d9baf1da6370cfdf87bfa3dd
Nicolas Chambrier <naholyr@gmail.com>
2011-05-10 18:25:04 +0200

    Reverted the souple management of dependency: better for user to simply npm install jus-task so that binaries & co work out of the box

--- tasks/task.generate.js
+++ tasks/task.generate.js
@@ -3,12 +3,7 @@ const fs = require('fs');
 const tasks = require('..');
 
 const TEMPLATE = ""
-  + "{{WARNING}}"
-  + "\n"
-  + "// Require tasks manager module, the best one has been selected depending on your environment\n"
-  + "{{COMMENT_JUS_TASK}}const tasks = require('{{PATH_JUS_TASK}}'); // Direct dependency with 'jus-task'\n"
-  + "{{COMMENT_JUS}}const tasks = require('{{PATH_JUS}}').tasks; // Got from dependency with 'jus'\n"
-  + "\n"
+  + "const tasks = require('jus-task');\n"
   + "const helper = tasks.helper;\n"
   + "const Task = tasks.Task;\n"
   + "\n"
@@ -60,83 +55,28 @@ module.exports = new tasks.Task({
       "{{NAME}}":             args.name,
       "{{NAMESPACE}}":        args.namespace,
       "{{ALIASES}}":          aliases,
-      "{{COMMENT_JUS_TASK}}": '// ',
-      "{{PATH_JUS_TASK}}":    'jus-task',
-      "{{COMMENT_JUS}}":      '// ',
-      "{{PATH_JUS}}":         'jus',
-      "{{WARNING}}":          '',
-    }
-    var found = checkJusTaskDependency();
-    if (found) {
-      var isGlobal = found.charAt(0) == '/';
-      if (isGlobal) {
-        // Add warning
-        tokens['{{WARNING}}'] = 'console.log("\x1b[1;33mYou should install dependencies locally and edit ' + file + ' to fix require()\x1b[0m");\n';
-      }
-      if (path.basename(found) == 'jus') { // jus
-        tokens['{{PATH_JUS}}'] = found;
-        tokens['{{COMMENT_JUS}}'] = '';
-      } else { // jus-task
-        tokens['{{PATH_JUS_TASK}}'] = found;
-        tokens['{{COMMENT_JUS_TASK}}'] = '';
-      }
-    } else {
-      tokens['{{WARNING}}'] = 'console.log("\x1b[1;37;41mYou must edit ' + file + ' to add the proper require()\x1b[0m");\nreturn false;\n';
-    }
-    switch (found) {
-      case 'local':
-        tokens['{{COMMENT_LOCAL}}'] = '';
-        tokens
-        break;
-      case 'jus':
-        tokens['{{COMMENT_JUS}}'] = '';
-        break;
-      default:
-        tokens['{{COMMENT_GLOBAL}}'] = '';
-        tokens['{{PATH_JUS_TASK}}'] = found;
-        break;
     }
     tasks.helper.token(file, tokens, TEMPLATE);
+    hintJusTaskDependency();
   }
 });
 
-function checkJusTaskDependency() {
-  var globalPrefix = '/usr/local/lib'; // TODO use npm to find prefix
-  function findModule(module, root) {
-    root = root || '.';
-    // Check local installation
-    if (!path.existsSync(path.join(root, path.join('node_modules', module)))) {
-      // module is not found
-      if (!path.existsSync('package.json')) {
-        // no package.json found
-        tasks.helper.log('Dependency to ' + module + ': No "package.json" found', tasks.helper.level.WARNING);
-        return 'no package.json';
-      } else {
-        var packageJson = JSON.parse(fs.readFileSync(path.join(root, 'package.json')));
-        if (typeof packageJson.dependencies[module] == 'undefined') {
-          // package.json found, but module is not declared as a dependency
-          tasks.helper.log('Dependency to ' + module + ': Dependency not found in "package.json"', tasks.helper.level.WARNING);
-          return 'no dependency';
-        } else {
-          // package.json found, and module is declared as a dependency: notice user to npm install ;)
-          tasks.helper.log('Dependency to ' + module + ': Dependency found in "package.json", but module not found, run "npm install"', tasks.helper.level.WARNING);
-          return 'no npm install';
-        }
-      }
+function hintJusTaskDependency() {
+  // Check local installation
+  if (!path.existsSync(path.join('node_modules', 'jus-task'))) {
+    // module is not found
+    if (!path.existsSync('package.json')) {
+      // no package.json found
+      tasks.helper.log('Failed resolving dependency to jus-task, and no "package.json" found: you should "npm install jus-task" or "npm init" and declare dependency', tasks.helper.level.WARNING);
     } else {
-      // Module is found locally
-      tasks.helper.logSection('Dependency', 'Will use ' + module + ' from ' + root + ' (you can ignore previous warnings)');
-      return 'yes';
+      var packageJson = JSON.parse(fs.readFileSync('package.json'));
+      if (typeof packageJson.dependencies['jus-task'] == 'undefined') {
+        // package.json found, but module is not declared as a dependency
+        tasks.helper.log('Failed resolving dependency to jus-task, you should edit your "package.json" and add dependency to "jus-task", then "npm install"', tasks.helper.level.WARNING);
+      } else {
+        // package.json found, and module is declared as a dependency: notice user to npm install ;)
+        tasks.helper.log('Failed resolving dependency to jus-task, but the declaration has been found in your "package.json", maybe you forgot to "npm install" ?', tasks.helper.level.WARNING);
+      }
     }
   }
-  // Check local modules
-  if (findModule('jus-task', process.cwd()) == 'yes') return 'jus-task';
-  if (findModule('jus', process.cwd())      == 'yes') return 'jus';
-  // Not found locally: search globally (as this is not a good idea, we'll add a warning)
-  tasks.helper.log('Dependencies: "jus" or "jus-task" not found locally, try globally…', tasks.helper.level.WARNING);
-  if (findModule('jus-task', globalPrefix)  == 'yes') return path.join(globalPrefix, path.join('node_module', 'jus'));
-  if (findModule('jus', globalPrefix)       == 'yes') return path.join(globalPrefix, path.join('node_module', 'jus'));
-  // Nope, you really need to install it
-  tasks.helper.log('Dependencies: Install "jus" or "jus-task" and edit the generated task to resolve dependency !', tasks.helper.level.ERROR);
-  return false;
 }
